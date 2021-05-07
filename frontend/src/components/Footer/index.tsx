f928fe41c6c7b92119f3cf4af87aca86fa9d5770
Benjamim Gomes <bejagomesvg@gmail.com>
2021-05-06 23:04:29 -0400

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-2 bg-dark text-white">
+      <div className="container">
+        <span>Developed on Spring React Week 3 by&nbsp;
+          <a className="text-info" href="https://github.com/bejagomesvg/sds3">Benjamim Gomes.</a>
+          <br />
+          <small className="text-light">School-sponsored event DevSuperior:&nbsp;
+            <a href="https://instagram.com/devsuperior.ig">@devsuperior.ig</a>
+          </small>
+        </span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
