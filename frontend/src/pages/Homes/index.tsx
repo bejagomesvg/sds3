aaa56d2bb339cd5dfef687f5a8e46822af822376
Benjamim Gomes <60727908+bejagomesvg@users.noreply.github.com>
2021-05-08 16:51:31 -0400

    Home, navigation

--- frontend/src/pages/Homes/index.tsx
+++ frontend/src/pages/Homes/index.tsx
@@ -1,13 +1,27 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
 
   return (
+    <>
+      <NavBar />
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">DSVendas</h1>
+          <p className="lead">Analyze your sales performance from different perspectives</p>
+          <hr />
+          <p>This application consists of displaying a dashboard from data provided by a backend built with Spring Boot..</p>
 
-    <div>
+          <Link className="btn btn-primary btn-lg" to="/dashboard">
+            Access deshboard
+          </Link>
 
-      <h1>Página Home</h1>
-
-    </div>
-
+        </div>
+      </div>
+      <Footer />
+    </>
   )
 
 }
